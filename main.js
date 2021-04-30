67fc3029172cf3483dad3facd8fe96293b76d840
MobileMars76874 <72148112+MobileMars76874@users.noreply.github.com>
2021-04-30 16:36:29 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+music = "";
+
+function preload(){
+music = loadSound("Billy Goat Stomp - Joel Cummins.mp3");
+}
+
+function setup(){
+canvas = createCanvas(600, 600);
+canvas.center();
+Video = createCapture(VIDEO);
+Video.hide();
+
+poseNet = ml5.poseNet(Video, model_loaded);
+
+}
+
+
+function play_s(){
+music.play();
+music.setVolume(1);
+music.rate(1);
+}
+
+function draw(){
+}
