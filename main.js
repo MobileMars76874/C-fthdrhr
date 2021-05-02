276e8ed57a0fed8467904854da6f5ce54dcaf4e7
MobileMars76874 <72148112+MobileMars76874@users.noreply.github.com>
2021-05-02 17:29:59 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 scoreleftWrist = 0;
+scorerightWrist = 0;
 left_wrist_x = "";left_wrist_y = "";
 right_wrist_x = "";right_wrist_y = "";
 music = "";
@@ -23,13 +24,30 @@ image(Video, 0, 0, 400, 400);
 
 fill(0, 255, 255);
 stroke(30, 255, 255);
+
+if(scorerightWrist > 0.2){
+circle(right_wrist_x, right_wrist_y, 20);
+if(right_wrist_y >= 0 && right_wrist_y <= 100){
+music.rate(0.5);
+}
+else if(right_wrist_y > 100 && right_wrist_y <= 200){
+music.rate(1);
+}
+else if(right_wrist_y > 200 && right_wrist_y <= 300){
+musci.rate(1.5);
+}
+else if(right_wrist_y > 300 && right_wrist_y <= 400){
+music.rate(2);
+}
+}
+
 if(scoreleftWrist > 0.2){
 circle(left_wrist_x, left_wrist_y, 20);
 numberleftWrist_y = Number(left_wrist_y);
 removeDecimals = floor(numberleftWrist_y);
 volume = removeDecimals/500;
 document.getElementById("V").innerHTML = "Volume : "+volume;
-setVolume(volume);
+music.setVolume(volume);
 }
 }
 
@@ -38,7 +56,9 @@ function gopose(results){
 if(results.length > 0){
 console.log(results);
 scoreleftWrist = results[0].pose.keypoints[9].score;
-console.log("Score : "+scoreleftWrist)
+console.log("Score left Wrist : "+scoreleftWrist);
+scorerightWrist = results[0].pose.keypoints[10].score;
+console.log("Score right Wrist : "+scorerightWrist);
 
 left_wrist_x = results[0].pose.leftWrist.x;
 left_wrist_y = results[0].pose.leftWrist.y;
